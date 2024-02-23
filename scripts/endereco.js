cabf9eaf4024c0286bc8b8fd5342d2d9e4581116
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2024-02-23 14:35:58 -0300

    Create endereco.js
    Funções de abrir e fechar para os popup de termos de uso e políticas de privacidade.

---
+++ scripts/endereco.js
@@ -0,0 +1,31 @@
+function fecharAbaTermos(){
+    
+    let termosDeUso = document.getElementById('termos-de-uso')
+    termosDeUso.style.display = 'none'
+    let opacidade = document.getElementById('opacidade')
+    opacidade.style.display = 'none'
+}
+
+function abrirAbaTermos(){
+    
+    let termosDeUso = document.getElementById('termos-de-uso')
+    termosDeUso.style.display = 'block'
+    let opacidade = document.getElementById('opacidade')
+    opacidade.style.display = 'block'
+}
+
+function fecharAbaPrivacidade(){
+    
+    let termosDeUso = document.getElementById('politica-privacidade')
+    termosDeUso.style.display = 'none'
+    let opacidade = document.getElementById('opacidade')
+    opacidade.style.display = 'none'
+}
+
+function abrirAbaPrivacidade(){
+    
+    let termosDeUso = document.getElementById('politica-privacidade')
+    termosDeUso.style.display = 'block'
+    let opacidade = document.getElementById('opacidade')
+    opacidade.style.display = 'block'
+}

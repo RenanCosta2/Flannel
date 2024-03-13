20564a0a7552f26f33309f04bf5201900b424072
Pedro Vinícius <110605121+queirozPedro@users.noreply.github.com>
2024-03-13 14:42:37 -0300

    Delete django-quickproject-main directory

--- django-quickproject-main/estacionamentos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from estacionamentos.models import Estacionamento, Endereco
-
-# Register your models here.
-
-admin.site.register(Estacionamento)
-admin.site.register(Endereco)
--- django-quickproject-main/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from users.models import Empresa, Gerente, Funcionario
-
-# Register your models here.
-
-admin.site.register(Empresa)
-admin.site.register(Gerente)
-admin.site.register(Funcionario)

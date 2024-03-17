88d3f972aac266b959c62db917b802b1a31ca639
Sr-Cardoso <nathancar.linhares77@gmail.com>
2024-03-16 22:40:18 -0300

    modificação: trocando CharFiel por EmailField no app users

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class GerenteGeral(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome completo', blank=False, null=False, unique=False)
     cpf = models.CharField(max_length=11, verbose_name='Cadastro de Pessoa Física', blank=False, null=False, unique=True)
     telefone = models.CharField(max_length=11, verbose_name='Telefone', null=False, unique=True, blank=False)
-    email = models.CharField(max_length=100, verbose_name='E-mail', blank=False, null=False, unique=True)
+    email = models.EmailField(max_length=100, verbose_name='E-mail', blank=False, null=False, unique=True)
     senha = models.CharField(max_length=50, verbose_name='Senha', blank=False, null=False, unique=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -21,7 +21,7 @@ class GerenteLocal(models.Model):
     nome = models.CharField(max_length=100, verbose_name='Nome completo', blank=False, null=False, unique=False)
     cpf = models.CharField(max_length=11, verbose_name='Cadastro de Pessoa Física', blank=False, null=False, unique=True)
     telefone = models.CharField(max_length=11, verbose_name='Telefone', null=False, unique=True, blank=False)
-    email = models.CharField(max_length=100, verbose_name='E-mail', blank=False, null=False, unique=True)
+    email = models.EmailField(max_length=100, verbose_name='E-mail', blank=False, null=False, unique=True)
     senha = models.CharField(max_length=50, verbose_name='Senha', blank=False, null=False, unique=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 

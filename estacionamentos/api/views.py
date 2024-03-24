cc99765e8a1e3bc80f3e1816e14604fd1ab37aba
Sr-Cardoso <nathancar.linhares77@gmail.com>
2024-03-24 19:57:05 -0300

    modificação: comentários desnecessários apagados e listagem de estacionamentos vinculados a uma empresa especifica

--- estacionamentos/api/views.py
+++ estacionamentos/api/views.py
@@ -1,10 +1,21 @@
+
 from rest_framework.viewsets import ModelViewSet 
 from estacionamentos.models import Empresa, Estacionamento, Locacao
 from estacionamentos.api.serializers import EmpresaSerializer, EstacionamentoSerializer, LocacaoSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class EmpresaViewSet(ModelViewSet):
     queryset = Empresa.objects.all()
     serializer_class = EmpresaSerializer
+
+    @action(detail=True, methods=['GET'])
+    def estacionamentos(self, request, pk=None):
+        empresa = self.get_object()
+        seriealizer = EstacionamentoSerializer(Estacionamento.objects.all(), many=True)
+        return Response(seriealizer.data)
+    
+
 
 class EstacionamentoViewSet(ModelViewSet):
     queryset = Estacionamento.objects.all()

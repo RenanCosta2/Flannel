8c90d945f556712dcfb3d1e5d7b3283918081d78
Sr-Cardoso <nathancar.linhares77@gmail.com>
2024-03-24 22:34:18 -0300

    modificação: organizando o arquivo de urls

--- django_quickproject/urls.py
+++ django_quickproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -8,15 +8,19 @@ from estacionamentos.api.views import EmpresaViewSet, EstacionamentoViewSet, Loc
 
 router = DefaultRouter()
 
-router.register("GerenteGeral", GerenteGeralViewSet, basename="GerenteGeral")
-router.register("GerenteLocal", GerenteLocalViewSet, basename="GerenteLocal")
-router.register("api/empresas", EmpresaViewSet, basename="empresa-view-set")
-router.register("api/estacionamentos", EstacionamentoViewSet, basename="estacionamento-view-set")
-router.register("api/lotacoes", LocacaoViewSet, basename="lotacao-view-set")
+# Rotas dos gerentes
+router.register(r"GerenteGeral", GerenteGeralViewSet, basename="GerenteGeral")
+router.register(r"GerenteLocal", GerenteLocalViewSet, basename="GerenteLocal")
+
+# Rotas do estacionamento
+router.register(r"empresas", EmpresaViewSet, basename="empresas")
+router.register(r"estacionamentos", EstacionamentoViewSet, basename="estacionamentos")
+router.register(r"lotacoes", LocacaoViewSet, basename="lotação")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token-auth/", views.obtain_auth_token),
+    path('api/', include(router.urls)),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-]+router.urls
+]

5e9f43a05f9507232cdc5b415a43202010512186
Sr-Cardoso <nathancar.linhares77@gmail.com>
2024-03-26 00:06:12 -0300

    modificação: ocultando a senha no momento da listagem

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -6,6 +6,9 @@ class GerenteGeralSerializer(serializers.ModelSerializer):
     class Meta:
         model = GerenteGeral
         fields = ['id', 'nome', 'cpf', 'telefone', 'email', 'senha', 'user']
+        extra_kwargs = {
+            'senha': {'write_only': True}
+        }
 
 
 class GerenteLocalSerializer(serializers.ModelSerializer):
@@ -13,3 +16,7 @@ class GerenteLocalSerializer(serializers.ModelSerializer):
     class Meta:
         model = GerenteLocal
         fields = ['id', 'nome', 'cpf', 'telefone', 'email', 'senha', 'user']
+        
+        extra_kwargs = {
+            'senha': {'write_only': True}
+        }
